b92f4679b5be148550efed134c87a8e981f604ad
shivraj598 <timilsenashivraj598@gmail.com>
2026-09-02 15:29:56 +0545

    fix: simplify image configuration in next.config.ts by removing unused properties

--- next.config.ts
+++ next.config.ts
@@ -2,37 +2,12 @@ import type { NextConfig } from "next";
 import path from "node:path";
 
 const nextConfig: NextConfig = {
+  output: "export",
   turbopack: {
     root: path.join(process.cwd()),
   },
   images: {
-    deviceSizes: [360, 414, 640, 768, 1024, 1280],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 2678400,
-    qualities: [60, 70, 75, 90, 100],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "static.wixstatic.com",
-        port: "",
-        pathname: "/media/**",
-        search: "",
-      },
-      {
-        protocol: "https",
-        hostname: "github.com",
-        port: "",
-        pathname: "/shivraj598.png",
-        search: "",
-      },
-      {
-        protocol: "https",
-        hostname: "unavatar.io",
-        port: "",
-        pathname: "/twitter/**",
-        search: "",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
