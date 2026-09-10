98d3978b52e7007374fbd05a3ad71053b76850d4
shivraj598 <timilsenashivraj598@gmail.com>
2026-09-10 16:49:13 +0545

    feat: remove unnecessary vertical and horizontal lines in PullRequestsPage for a cleaner layout

--- src/app/pull-requests/page.tsx
+++ src/app/pull-requests/page.tsx
@@ -11,10 +11,6 @@ export default function PullRequestsPage() {
       {/* Right Side Blueprint Navigation */}
       <RightNavbar />
 
-      {/* Vertical Lines - Ultra-fine Micro Dots */}
-      <div className="absolute top-0 bottom-0 left-[30%] w-0 border-r border-black/30 dark:border-white/[0.15] pointer-events-none hidden md:block" style={{ maskImage: 'repeating-linear-gradient(to bottom, black 0, black 1px, transparent 1px, transparent 6px)', WebkitMaskImage: 'repeating-linear-gradient(to bottom, black 0, black 1px, transparent 1px, transparent 6px)' }} />
-      <div className="absolute top-0 bottom-0 right-[30%] w-0 border-r border-black/30 dark:border-white/[0.15] pointer-events-none hidden md:block" style={{ maskImage: 'repeating-linear-gradient(to bottom, black 0, black 1px, transparent 1px, transparent 6px)', WebkitMaskImage: 'repeating-linear-gradient(to bottom, black 0, black 1px, transparent 1px, transparent 6px)' }} />
-
       {/* Fixed Header: Banner + Back + Title */}
       <SiteHeader
         variant="subpage"
@@ -32,13 +28,6 @@ export default function PullRequestsPage() {
       >
         <div className="mt-4">
           <OpenSourceContributions isFullPage />
-        </div>
-        
-        {/* Bottom Separator */}
-        <div className="relative mt-8">
-          <div className="absolute left-[-100vw] right-[-100vw] h-0 border-b border-black/30 dark:border-white/[0.15] pointer-events-none" style={{ maskImage: 'repeating-linear-gradient(to right, black 0, black 1px, transparent 1px, transparent 6px)', WebkitMaskImage: 'repeating-linear-gradient(to right, black 0, black 1px, transparent 1px, transparent 6px)' }} />
-          <div className="absolute -left-4 w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] -translate-x-1/2 translate-y-[-1px] pointer-events-none z-20" />
-          <div className="absolute -right-4 w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] translate-x-1/2 translate-y-[-1px] pointer-events-none z-20" />
         </div>
       </motion.div>
     </div>
