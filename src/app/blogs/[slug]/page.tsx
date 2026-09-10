a7a5b05047a540cbbda7b7d7101fd3c4b22740b0
shivraj598 <timilsenashivraj598@gmail.com>
2026-09-10 16:52:25 +0545

    feat: remove unnecessary vertical and dashed lines in BlogPostPage for a cleaner layout

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -44,10 +44,6 @@ export default async function BlogPostPage({ params }: { params: Promise<{ slug:
       {/* Right Side Blueprint Navigation */}
       <RightNavbar />
 
-      {/* Vertical Lines - Ultra-fine Micro Dots */}
-      <div className="absolute top-0 bottom-0 left-[30%] w-0 border-r border-black/30 dark:border-white/[0.15] pointer-events-none hidden md:block" style={{ maskImage: 'repeating-linear-gradient(to bottom, black 0, black 1px, transparent 1px, transparent 6px)', WebkitMaskImage: 'repeating-linear-gradient(to bottom, black 0, black 1px, transparent 1px, transparent 6px)' }} />
-      <div className="absolute top-0 bottom-0 right-[30%] w-0 border-r border-black/30 dark:border-white/[0.15] pointer-events-none hidden md:block" style={{ maskImage: 'repeating-linear-gradient(to bottom, black 0, black 1px, transparent 1px, transparent 6px)', WebkitMaskImage: 'repeating-linear-gradient(to bottom, black 0, black 1px, transparent 1px, transparent 6px)' }} />
-
       {/* Fixed Header: Banner + Back + Title */}
       <SiteHeader
         variant="subpage"
@@ -84,13 +80,6 @@ export default async function BlogPostPage({ params }: { params: Promise<{ slug:
               ))}
             </div>
           </div>
-        </div>
-
-        {/* Dashed Divider */}
-        <div className="relative mt-8 mb-2">
-          <div className="absolute left-[-100vw] right-[-100vw] h-0 border-b border-black/30 dark:border-white/[0.15] pointer-events-none" style={{ maskImage: 'repeating-linear-gradient(to right, black 0, black 1px, transparent 1px, transparent 6px)', WebkitMaskImage: 'repeating-linear-gradient(to right, black 0, black 1px, transparent 1px, transparent 6px)' }} />
-          <div className="absolute left-0 w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] -translate-x-1/2 translate-y-[-1px] pointer-events-none z-20" />
-          <div className="absolute right-0 w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] translate-x-1/2 translate-y-[-1px] pointer-events-none z-20" />
         </div>
 
         {/* Body */}
